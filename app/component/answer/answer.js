cb6ffec8b10db3e164de25b035372d77d07067e0
Megan Reardon <meganr@gmail.com>
2017-02-13 17:21:17 -0800

    have answers printing to individual question page

--- app/component/answer/answer.js
+++ app/component/answer/answer.js
@@ -11,4 +11,8 @@ module.exports = {
 
 function AnswerController($log, answerService) {
   $log.debug('AnswerController');
+
+  // $log.log(':::', answer);
+  // let answersList = questionViewCtrl.question.answers.$index;
+  // $log.log(':::', questionViewCtrl.question.answers.$index);
 }
