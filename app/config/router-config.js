6d852fa1f270e967aeaac29db890ad659d03f532
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2017-02-13 18:33:37 -0800

    add angular bootstrap ui

--- app/config/router-config.js
+++ app/config/router-config.js
@@ -3,15 +3,13 @@
 module.exports = ['$stateProvider', '$urlRouterProvider', routerConfig];
 
 function routerConfig($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.when('' , '/join#signup');
-  $urlRouterProvider.when('/' , '/join#signup');
   $urlRouterProvider.when('/signup' , '/join#signup');
   $urlRouterProvider.when('/login' , '/join#login');
 
   let states = [
     {
       name: 'home',
-      url: '/home',
+      url: '/',
       template: require('../view/home/home.html'),
       controller: 'HomeController',
       controllerAs: 'homeCtrl'
